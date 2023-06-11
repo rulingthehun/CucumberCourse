ece8798c9dc19ee48cb87043fbd42f57ce7f12e2
rulingthehun <114784395+rulingthehun@users.noreply.github.com>
2023-06-11 15:56:16 +0300

    Cucumber default build profile

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -7,10 +7,10 @@ import io.cucumber.testng.CucumberOptions;
         tags = "@SmokeTest", // hangi senaryolarda bu etiket varsa onlar çalıştırılacak
         features = {"src/test/java/FeatureFiles"},
         glue = {"StepDefinitions"},
-        plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
-        //plugin={"html:target/cucumber-html-report", "json:target/cucumber.json",
-        //        "pretty:target/cucumber-pretty.txt","usage:target/cucumber-usage.json",
-        //        "junit:target/cucumber-results.xml"}
+        //plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
+        plugin={"html:target/cucumber-html-report", "json:target/cucumber.json",
+                "pretty:target/cucumber-pretty.txt","usage:target/cucumber-usage.json",
+                "junit:target/cucumber-results.xml"}
 )
 public class _04_TestRunnerSmoke extends AbstractTestNGCucumberTests {
 }
